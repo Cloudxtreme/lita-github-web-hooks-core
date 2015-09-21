8cedd59817e8990ff1d2a0a0e747e010e5472d5b
Jim Van Fleet <jim.van.fleet@lattapartners.com>
2015-09-21 12:56:12 -0400

    All specs passing

--- spec/lita/extensions/github_web_hooks_core_spec.rb
+++ spec/lita/extensions/github_web_hooks_core_spec.rb
@@ -1,4 +1,8 @@
 require "spec_helper"
 
 describe Lita::Extensions::GithubWebHooksCore, lita: true do
+  it 'has a version number' do
+    expect(Lita::Github::WebHooks::Core::VERSION).not_to be nil
+  end
+  
 end
